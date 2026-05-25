4aa188013fa9af0590ed6cf210af2c1529d3a18a
KawtharDIDI <kawthar.boukahil@univ-constantine2.dz>
2026-05-25 12:02:45 +0100

    railway deploy setup

--- includes/db.php
+++ includes/db.php
@@ -1,19 +1,15 @@
 <?php
 
-$host = "localhost";
-$user = "taskuser";
-$password = "1234";
-$database = "task_manager";
-
 $conn = mysqli_connect(
-    $host,
-    $user,
-    $password,
-    $database
+    "zephyr.proxy.rlwy.net",
+    "root",
+    "DZASDKpuoeLOfkDKIMeTxPtkkKfNvnoc",
+    "railway",
+    32147
 );
 
 if(!$conn){
-    die(mysqli_connect_error());
+    die("Connection Failed: " . mysqli_connect_error());
 }
 
 ?>
